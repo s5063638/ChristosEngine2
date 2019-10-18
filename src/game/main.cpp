c512c3365e539bab224a73dad0d577b4c5c2ffa1
Christos Loukopoulos (s5063638) <s5063638@bournemouth.ac.uk>
2019-10-18 14:47:29 +0100

    Headers created for engine

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -7,15 +7,16 @@ using namespace game_engine;
 int main()
 {
 	//Initialize engine
-	
+	std::shared_ptr<Engine> engine = Engine::Init();
 	
 	//Create a single in-game object
-	
+	std::shared_ptr<Entity> entity = engine->AddEntity();
 
 	//Add a very simple component to it
 
 
 	//Start the main loop
+	engine->Start();
 
 	return 0;
 }
--- src/game_engine/Engine.h
+++ src/game_engine/Engine.h
@@ -1,4 +1,26 @@
+#ifndef _ENGINE_H_
+#define _ENGINE_H_
+
+#include <vector>
+#include <memory>
+
 namespace game_engine
 {
-	int temp;
+	class Entity;
+	class Keyboard;
+
+	class Engine
+	{
+		private:
+			std::vector<Entity> entities;
+			bool isRunning;
+			std::shared_ptr<Keyboard> keyboard;
+		public:
+			void Start();
+			void End();
+			std::shared_ptr<Entity> AddEntity();
+			static std::shared_ptr<Engine> Init();
+	};
 }
+
+#endif // !ENGINE_H_
